396009af741dbb116fdf8b6c56600fb5b8f853fc
Alexandre Abadie <alexandre.abadie@inria.fr>
2025-04-07 16:11:49 +0200

    /bin/bash: line 1: q: command not found

---
+++ bsp/helpers.h
@@ -0,0 +1,17 @@
+#ifndef __HELPERS_H
+#define __HELPERS_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+/** @brief Loop for a number of cycles.
+ *
+ *@param[in] cycles Number of cycles to loop for.
+**/
+static inline void busy_wait_cycles(uint32_t cycles) {
+    while (cycles--) {
+        __asm__ volatile ("":::);
+    }
+}
+
+#endif
