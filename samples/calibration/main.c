4c1a32bb5af8b2049b8c2eae4e7500864eb86f40
Alexandre Abadie <alexandre.abadie@inria.fr>
2025-04-16 14:16:26 +0200

    samples: introduce separate calibration application, make hello_world very simple

--- samples/calibration/main.c
+++ samples/calibration/main.c
@@ -1,18 +1,18 @@
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "helpers.h"
+#include "optical.h"
 
 // Number of for loop cycles between Hello World messages.
 // 700000 for loop cycles roughly correspond to 1 second.
 #define NUM_CYCLES_BETWEEN_TX (1000000UL)
 
 int main(void) {
-    puts("Welcome to SCuM!");
-    uint32_t g_tx_counter = 0;
+    perform_calibration();
 
+    uint32_t g_tx_counter = 0;
     while (1) {
         printf("Hello World! %lu\n", g_tx_counter++);
         busy_wait_cycles(NUM_CYCLES_BETWEEN_TX);
