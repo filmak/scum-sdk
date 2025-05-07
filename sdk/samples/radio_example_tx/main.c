63332cb8dbcb145e344492a7ae6fb9ee0d5c7675
filmak <fil@eecs.berkeley.edu>
2025-05-07 08:31:08 -0700

    radio tx - remove frequency sweep and leave LO on between transmissions so that it looks better on an SDR capture

--- sdk/samples/radio_example_tx/main.c
+++ sdk/samples/radio_example_tx/main.c
@@ -12,13 +12,12 @@
 // Number of for loop cycles between Hello World messages.
 // 700000 for loop cycles roughly correspond to 1 second.
 #define NUM_CYCLES_BETWEEN_TX (1000000UL)
-#define NUM_CYCLES_BETWEEN_PACKET (100UL)
+#define NUM_CYCLES_BETWEEN_PACKET (1000UL)
 #define TX_PACKET_LEN (64UL)
 
 void tx_endframe_callback(uint32_t timestamp);
 uint8_t packet[TX_PACKET_LEN] = {0};
 
-int main(void) {
 uint8_t fine_code = 0;
 uint8_t mid_code = 0;
 
@@ -39,7 +38,8 @@ int main(void) {
 }
 
 void tx_endframe_callback(uint32_t timestamp) {
-    radio_rfOff();
+//    radio_rfOff();
+    busy_wait_cycles(NUM_CYCLES_BETWEEN_PACKET);
     fine_code += 1;
     if (fine_code > 31) {
         fine_code = 0;
@@ -48,6 +48,8 @@ void tx_endframe_callback(uint32_t timestamp) {
     if (mid_code > 8) {
 	mid_code = 0;
     }
+
+    fine_code = 31;
 
     LC_FREQCHANGE(0,0,fine_code);
 
